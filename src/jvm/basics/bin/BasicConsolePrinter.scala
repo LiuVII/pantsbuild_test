3766162951d3aac4e8f75f738f1198aa51b3a718
LiuVII <mpyblake@gmail.com>
2018-09-10 21:18:03 -0700

    Add basic scala example with a build for binary

---
+++ src/jvm/basics/bin/BasicConsolePrinter.scala
@@ -0,0 +1,14 @@
+// Basic pants-scala project 2018 @LiuVII
+
+package jvm.basics.bin
+
+object BasicConsolePrinter {
+  def woo(): Unit = {
+    println("Wooooooooo!")
+  }
+
+  def main(): Unit = {
+    println("I'm running, nice!")
+    System.exit(0)
+  }
+}
